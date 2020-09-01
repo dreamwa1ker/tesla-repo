964e1ffed29dc4ce34b770ae5c6406f17b992573
dreamwa1ker <47291243+dreamwa1ker@users.noreply.github.com>
2020-09-01 15:01:42 +0300

    Create swift.swift

---
+++ swift.swift
@@ -0,0 +1,13 @@
+// Declare our new class 
+class Person {
+// We can define class property here
+var age  = 25
+// Implement Class initializer. Initializers are called when a new object of this class is created
+init() { 
+   print(“A new instance of this class Person is created.”) 
+ } 
+} 
+// We can now create an instance of class Person - an object - by putting parentheses after the class name
+let personObj =  Person()
+// Once an instance of Person class is created we can access its properties using the dot “.” syntax.
+print(“This person age is \(personObj.age)”)
